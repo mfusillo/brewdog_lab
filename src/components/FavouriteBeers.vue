4e3d93caecf2f15ea5f50136eab34ab12405b553
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-16 14:27:13 +0000

    add favourites component

--- src/components/FavouriteBeers.vue
+++ src/components/FavouriteBeers.vue
@@ -1,10 +1,20 @@
 <template lang="html">
 
+  <div>
+    <ul>
+      <li v-for="favouriteBeer of favouriteBeers">
+        <h2>{{ favouriteBeer.name }}</h2>
+        <img v-bind:src="favouriteBeer.image_url">
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'favourite-beers'
+  name: 'favourite-beers',
+  props: ['favouriteBeers']
 }
 </script>
 
