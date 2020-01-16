71cbfcb4811324f9011f2801845d69ecf4b85ce4
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-16 11:35:17 +0000

    add select component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <beer-select v-bind:beers="beers"></beer-select>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import BeerSelect from './components/BeerSelect.vue'
 
 export default {
   name: 'app',
+  data(){
+    return {
+      beers: []
+    }
+  },
+  mounted(){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(result => result.json())
+    .then(beers => this.beers = beers)
+  },
   components: {
-    HelloWorld
+    "beer-select": BeerSelect
   }
 }
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/BeerSelect.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <select v-model="selection">
+      <option v-for="beer of beers" v-bind:value="beer.id" v-bind:key="beer.id">{{ beer.name }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'beer-select',
+  props: ['beers'],
+  data(){
+    return {
+      selection: null
+    }
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
